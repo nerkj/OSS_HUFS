96674f310c61c7c63f77714eb7dfc5708c8da57b
nerkj <155232582+nerkj@users.noreply.github.com>
2024-05-30 04:30:04 +0000

    commit

---
+++ valid-date.sh
@@ -0,0 +1,76 @@
+#!/bin/bash
+
+mon=$1
+day=$2
+year=$3
+
+mon_lower=$(echo $mon | tr '[:upper:]' '[:lower:]' )
+if [ $# != 3 ]
+then
+	echo "입력값 오류"
+	exit 0
+fi
+case $mon_lower in
+    "jan" | "january" | 1 ) mon=1 ;;
+    "feb" | "february" |  2 ) mon=2 ;;
+    "mar" | "march" | 3 ) mon=3 ;;
+    "apr" | "april" | 4 ) mon=4 ;;
+    "may" | 5 ) mon=5 ;;
+    "jun" | "june" | 6 ) mon=6 ;;
+    "jul" | "july" | 7 ) mon=7 ;;
+    "aug" | "august" | 8 ) mon=8 ;;
+    "sep" | "september" | 9 ) mon=9 ;;
+    "oct" | "october" | 10 ) mon=10 ;;
+    "nov" | "november" | 11 ) mon=11 ;;
+    "dec" | "december" | 12 ) mon=12 ;;
+    *) echo "존재하지 않는 월입니다. "+ "$1 $day $year 유효하지 않습니다."; exit 1 ;;
+esac
+
+if [ $year -lt 0 ]
+then
+	echo "존재하지 않는 연도입니다. "+ "$1 $day $year 유효하지 않습니다."
+	exit 2
+elif (($mon % 2 == 1)) && [ $day -gt 31 ]
+then
+	echo "존재하지 않는 일입니다. "+ "$1 $day $year 유효하지 않습니다."
+	exit 3
+elif (($mon % 2 == 0)) && [ $day -gt 30 ]
+then
+	echo "존재하지 않는 일입니다. "+ "$1 $day $year 유효하지 않습니다."
+	exit 4
+fi
+
+if (($year % 4 == 0)) && (($year % 100 != 0)) || (($year % 400 == 0))
+then
+    if(( $mon == 2 )) && [ $day -gt 29 ]))
+    then
+        echo "존재하지 않는 일입니다. "+ "$1 $day $year 유효하지 않습니다." 
+        exit 5
+else
+    if(( $mon == 2 )) && [ $day -gt 28 ]))
+    then
+        echo "존재하지 않은 일입니다."+ "$1 $day $year 유효하지 않습니다."
+        exit 6
+fi
+
+case $mon_lower in
+    1) echo "January $day, $year" ;;
+    2) echo "Feb $day, $year" ;;
+    3) echo "Mar $day, $year" ;;
+    4) echo "Apr $day, $year" ;;
+    5) echo "May $day, $year" ;;
+    6) echo "Jun $day, $year" ;;
+    7) echo "Jul $day, $year" ;;
+    8) echo "Aug $day, $year" ;;
+    9) echo "Sep $day, $year" ;;
+    10) echo "Oct $day, $year" ;;
+    11) echo "Nov $day, $year" ;;
+    12) echo "Dec $day, $year" ;;
+esac
+
+    
+
+
+
+
+
